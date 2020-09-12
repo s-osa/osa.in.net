fe2af83dd30a82bf46dd0448786f6d6402dcf5e2
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-09-13 05:04:35 +0900

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-material-ui`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "osa.in.net",
+      },
+    },
+  ],
 }
