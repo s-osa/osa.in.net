f3b3965996aa7c59d7adad34da433c3153e1a8f5
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-09-13 04:39:10 +0900

    Brand link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
 import { Container, Typography } from "@material-ui/core"
 import Head from "./Head"
 
 const useStyles = makeStyles(theme => ({
+  titleLink: {
+    "text-decoration": "none",
+    color: "#000000",
+  },
   title: {
     margin: theme.spacing(0, 2),
   },
@@ -19,14 +24,16 @@ export default function Layout({ children, title }) {
     <Container maxWidth="sm" className={classes.container}>
       <Head title={title} />
 
-      <Typography
-        variant="h5"
-        component="h2"
-        gutterBottom
-        className={classes.title}
-      >
-        osa.in.net
-      </Typography>
+      <Link to={"/"} className={classes.titleLink}>
+        <Typography
+          variant="h5"
+          component="h2"
+          gutterBottom
+          className={classes.title}
+        >
+          osa.in.net
+        </Typography>
+      </Link>
 
       {children}
     </Container>
